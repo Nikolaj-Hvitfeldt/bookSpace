41b78816ab3a1675855db0bf5d131e583decb85a
Nikolaj-hvitfeldt <nhvitfeldt1@gmail.com>
2026-03-19 14:59:32 +0100

    feat: Book Schema implemented
    -Created book schema and validations. CoverImage is embedded into the books collection as they go hand in hand.
    
    Updated erdDiagram to have ratingsCount to reflect the seeding data - I would personally think that was more of an aggregate function when fetching data from the database, but for now lets stick with the provided seeding data

---
+++ app/db/models/Book.ts
@@ -0,0 +1,90 @@
+import mongoose, { Schema, model, Types, type InferSchemaType} from "mongoose";
+
+const coverImageSchema = new Schema({
+    url: { 
+        type: String, 
+        required: true 
+    },
+    width: { 
+        type: Number, 
+        required: true 
+
+    },
+    height: { 
+        type: Number, 
+        required: true 
+    },
+},
+{ _id: false }
+);
+
+const bookSchema = new Schema({
+    title: { 
+        type: String, 
+        trim: true,
+        required: [true, "Title is required"] 
+    },
+    author: { 
+        type: [String], 
+        trim: true,
+        required: [true, "Author(s) is required"] 
+    },
+    description: { 
+        type: String, 
+        trim: true,
+        default: "No description available"
+    },
+    releaseYear: { 
+        type: Number, 
+        default: 0,
+        min: 0,
+        max: new Date().getFullYear()
+    },
+    slug: { 
+        type: String,
+        required: [true, "Slug is required"],
+        lowercase: true,
+        trim: true,
+        unique: true, 
+    },
+    pageCount: { 
+        type: Number, 
+        default: 0,
+        min: 0 },
+    rating: { 
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 5,
+     },
+     ratingsCount: {
+        type: Number,
+        default: 0,
+        min: 0,
+     },
+     //Maybe change to enums down the line?
+    tags: {
+        type: [String], 
+        default: [] 
+    },
+    //Same as above
+    moods: {
+        type: [String], 
+        default: [] 
+    },
+    //same as above above
+    genres: { 
+        type: [String], 
+        default: [] 
+    },
+    coverImage: { 
+        type: coverImageSchema, 
+        required: true
+    },
+}, 
+{ timestamps: true }
+);
+
+export type BookType = InferSchemaType<typeof bookSchema> & { _id: Types.ObjectId };
+export default model<BookType>("Book", bookSchema);
+
