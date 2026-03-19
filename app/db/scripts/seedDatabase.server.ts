dfff07b58ef4a4c725ddbd2afa39036fb3adc639
Nikolaj-hvitfeldt <nhvitfeldt1@gmail.com>
2026-03-19 16:54:24 +0100

    feat: Simple login and auth via regular form
    -Added auth for users via remix-auth.
    - Added seeding script and seeding data for 2 test users
    - Added very simple login route to test if auth works properly

--- app/db/scripts/seedDatabase.server.ts
+++ app/db/scripts/seedDatabase.server.ts
@@ -1,9 +1,12 @@
 import Book from "../models/Book";
+import User from "../models/User";
 import books from "../seedingData/books.json";
-
+import users from "../seedingData/users.json";
+import bcrypt from "bcryptjs";
 export default async function seedDatabase() {
     try{
         await seedBooks();
+        await seedUsers();
         console.log("Database seeded successfully");
     } catch (error) {
         console.error("Error seeding database:", error);
@@ -18,4 +21,16 @@ async function seedBooks() {
     }
     await Book.insertMany(books);
     console.log("Books seeded successfully");
+}
+
+async function seedUsers() {
+    const usersCount = await User.countDocuments();
+    if (usersCount > 0) {
+        console.log("Users already seeded");
+        return;
+    }
+
+    //To make sure hashing is working - call create here instead of insertMany. This goes through the mongoose schema with hashing and salting.
+    await User.create(users);
+    console.log("Users seeded successfully");
 }
