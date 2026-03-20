aad6598ea5c6be9322f31dee6a0d169e72651edd
Nikolaj-hvitfeldt <nhvitfeldt1@gmail.com>
2026-03-20 12:06:42 +0100

    fix: testing afterEach approach changed
    -Now looks after an unique slug string (using Date.now) and deletes the books containing that string.
    
    - No longer deletes the entire books collection :)))))))

--- app/db/tests/book.model.test.ts
+++ app/db/tests/book.model.test.ts
@@ -3,28 +3,31 @@ import Book from "../models/Book";
 
 //Integration tests for the Book model
 
-const book = new Book({
-    title: "Test Book",
-    author: ["Test Author"],
-    description: "Desc",
-    releaseYear: 2024,
-    slug: "test book 1, the empire strikes forth",
-    pageCount: 150,
-    rating: 4.2,
-    ratingsCount: 10,
-    tags: ["Niko", "is", "cool"],
-    moods: ["Happy", "Sad"],
-    genres: ["Fantasy", "Romance", ],
-    coverImage: { url: "Super.sick.url.com", width: 340, height: 500 },
-});
+
+const testSlug = `unique-string-${Date.now()}`;
 
 describe("Book model", () => {
 
     afterEach(async () => {
-        await Book.deleteMany({book: Book});
+        await Book.deleteMany({slug: testSlug});
     });
 
   it("should create a new book", async () => {
-    await book.save();
+    const testBook = new Book({
+        title: "Test Book",
+        author: ["Test Author"],
+        description: "Desc",
+        releaseYear: 2024,
+        slug: testSlug,
+        pageCount: 150,
+        rating: 4.2,
+        ratingsCount: 10,
+        tags: ["Niko", "is", "cool"],
+        moods: ["Happy", "Sad"],
+        genres: ["Fantasy", "Romance", ],
+        coverImage: { url: "Super.sick.url.com", width: 340, height: 500 },
+    });
+    await testBook.save();
+    expect(testBook.id).toBeDefined();
   });
 });
