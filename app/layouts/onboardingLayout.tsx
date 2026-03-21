91e1b5c5e2b92345d62fcb8208d8d3efdc21409a
Nikolaj-hvitfeldt <nhvitfeldt1@gmail.com>
2026-03-21 22:52:34 +0100

    fix(ish): Workaround for the requireUser on onboarding as that currently not works as intended.
    -Outcommented requireUser for now, with the intention of reintroducing it when working on the register account part of the codebase

--- app/layouts/onboardingLayout.tsx
+++ app/layouts/onboardingLayout.tsx
@@ -1,20 +1,26 @@
 import { Outlet, redirect } from "react-router";
 import type { Route } from "./+types/onboardingLayout";
 import User from "../db/models/User";
-import { requireUser } from "~/services/auth.server";
+import { requireUser, getUserData } from "~/services/auth.server";
 
 export async function loader({ request }: Route.LoaderArgs) {
-    const user = await requireUser(request);
+   // const user = await requireUser(request);
   
-    const currentUser = await User.findById(user._id);
-    if (!currentUser) {
-      return redirect("/login");
-    }
+   // const currentUser = await User.findById(user._id);
+   // if (!currentUser) {
+   //   return redirect("/login");
+   // }
   
-    if (currentUser.onboardingComplete) {
-      return redirect("/");
-    }
-  }
+   // if (currentUser.onboardingComplete) {
+   //   return redirect("/");
+   // }
+
+  // Temporary fix for requireUser not working. Revisit when implementing register/sign up
+   const user = await getUserData(request);
+   if (user) {
+       return redirect("/onboarding/landing");
+   }
+}
   
   export default function OnboardingLayout() {
     return <Outlet />;
