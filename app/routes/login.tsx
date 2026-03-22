098916270d3ad8d9064074eb5820fb2b94dbe5b6
Nikolaj-hvitfeldt <nhvitfeldt1@gmail.com>
2026-03-23 00:27:36 +0100

    feat: Input component
    -Added Input component to ui components. A lot of copied code from button and searchfield component, as they are closely related visually

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,52 +3,7 @@ import type { Route } from "./+types/login";
 import { authenticator } from "../services/auth.server";
 import { Button } from "../components/ui/button";
 import { SearchBar } from "../components/ui/searchbar";
-
-export default function Login({ actionData }: Route.ComponentProps) {
-  return (
-    <div>
-      <h1>Login</h1>
-
-      <Form method="post" className="flex flex-col space-y-4">
-        <label className="block" htmlFor="email">
-          Email
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            className="border border-gray-300 rounded-md p-2"
-          />
-        </label>
-        <label className="block" htmlFor="password">
-          Password
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            className="border border-gray-300 rounded-md p-2"
-          />
-        </label>
-
-        {/* Show error message if there is one */}
-        {actionData?.error ? (
-          <div className="text-red-600">
-            <p>{actionData?.error}</p>
-          </div>
-        ) : null}
-        <div className="flex justify-center">
-          <Button type="submit">Primary Button</Button>
-        </div>
-        <div className="flex justify-center">
-          <Button type="submit" variant="secondary">
-            Secondary Button
-          </Button>
-        </div>
-      </Form>
-
-      <SearchBar placeholder="Search..." />
-    </div>
-  );
-}
+import { Input } from "../components/ui/input";
 
 export async function action({ request }: Route.ActionArgs) {
   try {
@@ -61,4 +16,37 @@ export async function action({ request }: Route.ActionArgs) {
   } catch (error) {
     return { error: "Invalid email or password" };
   }
+}
+
+const text = "Welcome,\nLog in to continue";
+
+export default function Login({ actionData }: Route.ComponentProps) {
+  return (
+    <div>
+      <div className="w-full text-left mt-[clamp(20px,4vh,32px)]">
+        <h1 className="onboarding-title whitespace-pre-line">{text}</h1>
+      </div>
+
+      <div className="flex flex-col mt-[clamp(20px,4vh,32px)]">
+        <Form method="post" className="flex flex-col space-y-4">
+          <Input
+            label="Email"
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="border border-gray-300 rounded-md p-2"
+          />
+          <Input
+            label="Password"
+            id="password"
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="border border-gray-300 rounded-md p-2"
+          />
+        </Form>
+      </div>
+    </div>
+  );
 }
