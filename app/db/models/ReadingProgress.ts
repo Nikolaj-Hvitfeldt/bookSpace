2c93fa5cd153c8e8c918dab38d2183b5119183e0
Nikolaj-hvitfeldt <nhvitfeldt1@gmail.com>
2026-03-24 15:31:21 +0100

    feat: ReadingProgress model added

---
+++ app/db/models/ReadingProgress.ts
@@ -0,0 +1,37 @@
+import { Schema, model, Types, type InferSchemaType } from "mongoose";
+
+const readingProgressSchema = new Schema(
+  {
+    user: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    book: {
+      type: Types.ObjectId,
+      ref: "Book",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["currently-reading", "completed", "want-to-read"],
+      default: "currently-reading",
+    },
+    currentPage: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+export type ReadingProgressType = InferSchemaType<
+  typeof readingProgressSchema
+> & {
+  _id: Types.ObjectId;
+};
+export default model<ReadingProgressType>(
+  "ReadingProgress",
+  readingProgressSchema,
+);
