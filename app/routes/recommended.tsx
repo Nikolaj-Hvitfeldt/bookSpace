f869f5e6a059eb85c16712a60895404433fe9024
Nikolaj-hvitfeldt <nhvitfeldt1@gmail.com>
2026-03-25 02:34:18 +0100

    feat: Added Bookmark to all routes

--- app/routes/recommended.tsx
+++ app/routes/recommended.tsx
@@ -1,11 +1,22 @@
 import BookListPage, { type BookList } from "~/components/books/BookListPage";
 import { getRecommendedBooksList } from "~/db/queries/books.server";
 import type { Route } from "./+types/popular";
+import { bookmarkAction } from "~/actions/bookmark.server";
+import { getUser } from "~/services/auth.server";
+import { getFavoriteBooks } from "~/db/queries/favorites.server";
+import { markBooksAsBookmarked } from "~/db/queries/favorites.server";
 
-export async function loader() {
+export async function action({ request }: Route.ActionArgs) {
+  return bookmarkAction(request);
+}
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const user = await getUser(request);
+  const favorittedBooks = await getFavoriteBooks(user?._id.toString() ?? "");
   const books = await getRecommendedBooksList();
+  const booksWithFavoriteFlags = markBooksAsBookmarked(books, favorittedBooks);
 
-  return { books };
+  return { books: booksWithFavoriteFlags };
 }
 
 export default function Recommended({ loaderData }: Route.ComponentProps) {
--- app/routes/shortEscapes.tsx
+++ app/routes/shortEscapes.tsx
@@ -1,11 +1,22 @@
 import BookListPage, { type BookList } from "~/components/books/BookListPage";
 import { getShortBooksList } from "~/db/queries/books.server";
 import type { Route } from "./+types/popular";
+import { getUser } from "~/services/auth.server";
+import { getFavoriteBooks } from "~/db/queries/favorites.server";
+import { markBooksAsBookmarked } from "~/db/queries/favorites.server";
+import { bookmarkAction } from "~/actions/bookmark.server";
 
-export async function loader() {
+export async function action({ request }: Route.ActionArgs) {
+  return bookmarkAction(request);
+}
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const user = await getUser(request);
+  const favorittedBooks = await getFavoriteBooks(user?._id.toString() ?? "");
   const books = await getShortBooksList();
+  const booksWithFavoriteFlags = markBooksAsBookmarked(books, favorittedBooks);
 
-  return { books };
+  return { books: booksWithFavoriteFlags };
 }
 
 export default function ShortEscapes({ loaderData }: Route.ComponentProps) {
