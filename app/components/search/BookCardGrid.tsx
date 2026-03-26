dbd74f10a3b3f09a3cbb922b4dcc1a9c027082ae
Nikolaj-hvitfeldt <nhvitfeldt1@gmail.com>
2026-03-26 03:24:49 +0100

    refactor: searchResultCard
    -Refactored searchResultCard into BookCardGrid as i think that would be more advantagerous
    
    -Moved BookCardGrid into its own component file, as i see potential in using it in onboarding

---
+++ app/components/search/BookCardGrid.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router";
+import type { BookList } from "~/types/bookList";
+import BookCard from "../books/BookCard";
+
+type BookCardGridProps = {
+  books: BookList[];
+  maxBooks?: number;
+};
+export default function BookCardGrid({
+  books,
+  maxBooks = 6,
+}: BookCardGridProps) {
+  const shown = books.slice(0, maxBooks);
+  return (
+    <ul className="grid grid-cols-3 gap-[10px]">
+      {shown.map((book) => {
+        if (!book.bookSlug) return null;
+
+        return (
+          <li key={book.id} className="shrink-0">
+            <Link to={`/books/${book.bookSlug}`} className="inline-block">
+              <BookCard
+                title={book.title}
+                coverImage={book.coverImage}
+                progressPercentage={book.progressPercentage}
+              />
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
